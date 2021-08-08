1214b0cdbe26ce0e5882ff036c1d375c629b997d
zakariasamy <zakariasamy10@gmail.com>
2021-08-08 05:05:30 +0200

    1. add .env.example 2. change config to get data from .env

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
 return [
-    'host' => '127.0.0.1',
-    'port' => 3306,
-    'database' => 'dbname',
-    'username' => 'username',
-    'password' => 'password',
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
+    'host' => getenv('host'),
+    'port' => getenv('port'),
+    'database' => getenv('database'),
+    'username' => getenv('username'),
+    'password' => getenv('password'),
+    'charset' => getenv('charset'),
+    'collation' => getenv('collation'),
 ];
+
